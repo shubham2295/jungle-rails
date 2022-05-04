f2affd376e8447acdde56306bcaccddb09b31012
shubhampatel <shubhampatel2295@gmail.com>
2022-05-04 15:50:30 -0600

    Added inventory dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,10 @@
 class Admin::DashboardController < ApplicationController
+
   def show
+
+    @total_products = Product.sum(:quantity)
+    @total_categories = Category.count()
+
   end
+
 end
--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,6 +1,6 @@
 class Admin::ProductsController < ApplicationController
 
-  http_basic_authenticate_with name: "jungle", password: "book"
+  http_basic_authenticate_with name: ENV["HTTP_BASIC_USER"], password: ENV["HTTP_BASIC_PASSWORD"]
 
   def index
     @products = Product.order(id: :desc).all
